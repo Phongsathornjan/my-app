642506522bb1958aa0f6f27e64db0c051a359c88
Phongsathorn Janjamsai <Phongsathornjanjamsai@gmail.com>
2024-08-05 10:29:11 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import react from 'react'
+import './style.css'
+
+
+export default function App (){
+
+  return (
+    <>
+    <h2>Hi DeveloperThai.com</h2>
+    <div className='title' >React & React Native</div>
+    </>
+  )
+}
+
+
+
