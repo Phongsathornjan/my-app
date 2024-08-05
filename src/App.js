4b05ee98f6e0457030218c20de9c4a3c34931b70
Phongsathorn Janjamsai <Phongsathornjanjamsai@gmail.com>
2024-08-05 11:44:16 +0700

    update class-components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import react from 'react'
 import './style.css'
+import Calendar from './class-components'
 
 
 export default function App (){
 
   return (
     <>
+    <center>
     <h2>Hi DeveloperThai.com</h2>
     <div className='title' >React & React Native</div>
+    </center>
+    <Calendar></Calendar>
     </>
   )
 }
