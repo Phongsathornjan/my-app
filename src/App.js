3d73fffd361c146177efaf5e5e74a3fbcb331da2
Phongsathornjan <Phongsathornjanjamsai@gmail.com>
2024-08-11 14:54:23 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import react from 'react'
 import './style.css'
 import Calendar from './class-components'
+import { Header, Content, Footer } from './func-components'
 
 
 export default function App (){
 
   return (
     <>
-    <center>
-    <h2>Hi DeveloperThai.com</h2>
-    <div className='title' >React & React Native</div>
-    </center>
+    <Header></Header>
     <Calendar></Calendar>
+    <Content></Content>
+    <Footer></Footer>
     </>
   )
 }
